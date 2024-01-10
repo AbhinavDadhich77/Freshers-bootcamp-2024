d48923de521bfe1b9eec0d953734da37a6462e70
AbhinavDadhich77 <94181689+AbhinavDadhich77@users.noreply.github.com>
2024-01-10 15:32:15 +0530

    Create OOP's_filter.py

---
+++ paradigms/OOP's_filter.py
@@ -0,0 +1,28 @@
+class ConsoleDisplayController:
+    __content = ''
+    def setContent(self, message):
+        self.__content = message
+    def display(self):
+        print(self.__content)
+        
+class StartsWithStrategy:
+    __startsWith = ''
+    def setStartsWith(self, key):
+        self.__startsWith = key
+    def invoke(self, item):
+        if item[0].lower() == self.__startsWith.lower():
+            return item
+            
+class StringListFilterController:
+    def filter(self,strList):
+        for message in strList:
+            startsWith = StartsWithStrategy()
+            startsWith.setStartsWith('a')
+            Element = ConsoleDisplayController()
+            if startsWith.invoke(message):
+                Element.setContent(startsWith.invoke(message))
+                Element.display()
+ 
+arrayOfStrings = ['ayan','deep','aman','abhinav']
+StringListFilter = StringListFilterController()
+StringListFilter.filter(arrayOfStrings)
